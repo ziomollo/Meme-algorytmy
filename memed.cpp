1e1126590177ce38f39015d48fd2c7bf8ed5f243
ziomollo <ziomollo@gmail.com>
2017-10-28 21:48:08 +0200

    Update memed.cpp

--- memed.cpp
+++ memed.cpp
@@ -1,8 +1,23 @@
 #include<iostream>
 #include<cmath>
 using std::cout;
+using std::endl;
+using std::cin;
 
-bool euklides(int a,int b)
+bool modulo_exponent(unsigned short base, unsigned short exponent, unsigned short mod)
+{
+    unsigned short result = 1;
+    while (exponent > 0)
+    {
+        if (exponent % 2 == 1)
+            result = (result * base) % mod;
+        exponent = exponent >> 1;
+        base = (base * base) % mod;
+    }
+    return (bool)(result - 1);
+}
+
+bool euklides(unsigned short a,unsigned short b)
 {
    while (a != b)
     {
@@ -18,37 +33,25 @@ bool euklides(int a,int b)
     return false;
 }
 
-bool prime(int a)
+bool prime(unsigned short a)
 {
-    for(int i = 2 ; i * i <= a ; ++i)
+    for(unsigned short i = 2 ; i * i <= a ; ++i)
         if(a % i == 0)
             return false;
 
     return true;
 }
 
-bool divided(int a, int b)
-{
-    int p = pow(a,b-1);
-    p -= 1;
 
-
-    cout << p << "\n";
-    if(p == 0)
-        return true;
-
-    return false;
-}
-
-bool karmelowa(int a)
+bool karmelowa(unsigned short a)
 {
     if(prime(a))
         return false;
 
-    for(int i=2; i < a; ++i)
+    for(unsigned short i=2; i < a; ++i)
     {
         if(euklides(a,i))
-            if(!divided(i,a))
+            if(modulo_exponent(i,a-1,a))
                 return false;
     }
 
@@ -57,14 +60,24 @@ bool karmelowa(int a)
 int main()
 {
 
-    cout << std::boolalpha;
-    int a = 1729;
+    unsigned short tab[10];
+    unsigned short n,i = 0;
 
+    while(cin >> n && n != 0)
+    {
+        tab[i] = n;
+        ++i;
+    }
 
-
-    cout << divided(2,561);
-    //cout << karmelowa(1729);
-
-   ;
-
+    for(int j = 0 ; j <= i-1 ; ++j)
+    {
+        if(karmelowa(tab[j]))
+        {
+            cout << "The number " << tab[j] << " is a Carmichael number.\n";
+        }
+        else
+        {
+            cout << tab[j] << " is normal.\n";
+        }
+    }
 }
